9130b6b6fa9a58722f7405b2a5461af6323e4979
Michael Dobachesky <mgdobachesky@gmail.com>
2018-08-18 10:13:07 -0400

    Refine settings file

--- maint-1.0.0/settings.sh
+++ maint-1.0.0/settings.sh
@@ -1,8 +1,18 @@
-# Directory where currently installed AUR packages are stored
-export AURDEST="$HOME.aur"
-
-# Directories where broken symlinks should be searched for
-export SYMLINKS_CHECK=("/etc" "/home" "/opt" "usr")
-
 # Country to generate the mirror list for
 export MIRRORLIST_COUNTRY='United States'
+
+# Directory where currently installed AUR packages are stored
+export AURDEST="$HOME/.aur"
+
+# Directories where broken symlinks should be searched for
+export SYMLINKS_CHECK=("/bin"
+                       "/etc"
+                       "/home"
+                       "/lib"
+                       "/lib64"
+                       "/opt"
+                       "/srv"
+                       "/usr")
+
+# Directories to exclude from backup
+export BACKUP_EXCLUDE=()
