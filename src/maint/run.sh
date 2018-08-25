63ac2f4bf05258937addb9b609296ca4b8b95bfe
Michael Dobachesky <mgdobachesky@gmail.com>
2018-08-25 11:51:38 -0400

    get rid of extra tabs

--- src/maint/run.sh
+++ src/maint/run.sh
@@ -190,13 +190,13 @@ source {{PKG_PATH}}/settings.sh
 PS3='Action to take: '
 select opt in "Arch Linux News" "Upgrade System" "Clean Filesystem" "System Error Check" "Backup System" "Update Settings" "Exit"; do
     case $REPLY in
-        1) fetch_news;;
-        2) system_upgrade;;
-        3) system_clean;;
+		1) fetch_news;;
+		2) system_upgrade;;
+		3) system_clean;;
 		4) system_errors;;
 		5) backup_system;;
 		6) update_settings;;
-        7) break;;
-        *) echo "Please choose an existing option";;
+		7) break;;
+		*) echo "Please choose an existing option";;
     esac
 done
