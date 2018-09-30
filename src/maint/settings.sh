409096816dc1adff91e87792f3b6c5de21147bc0
Michael Dobachesky <mgdobachesky@gmail.com>
2018-09-29 22:25:02 -0400

    Add package 1.11

--- src/maint/settings.sh
+++ src/maint/settings.sh
@@ -4,10 +4,10 @@
 export MIRRORLIST_COUNTRY='United States'
 
 # Directory where currently installed AUR packages are stored
-export AUR_DIR="$HOME/.aur/"
+export AUR_DIR="$HOME/.aur"
 
 # Where to store the system backup
-export BACKUP_LOCATION="/usr/local/backup/"
+export BACKUP_LOCATION="/usr/local/backup"
 
 # Directories in which broken symlinks should be searched
 export SYMLINKS_CHECK=("/bin"
