7e1361b2b5843e0c1808d38cbc3cfa6d34f3fd3b
Michael Dobachesky <mgdobachesky@gmail.com>
2019-04-21 12:16:16 -0400

    Improve organization of settings

--- src/maint/settings.sh
+++ src/maint/settings.sh
@@ -1,3 +1,7 @@
+#######################################################################
+######################## Customization Options ########################
+#######################################################################
+
 # User interface of choice (cli, dialog)
 export USER_INTERFACE='dialog'
 
@@ -7,6 +11,14 @@ export SETTINGS_EDITOR='vim'
 # Country to generate the mirror list for
 export MIRRORLIST_COUNTRY='United States'
 
+
+#######################################################################
+############################# AUR Options #############################
+#######################################################################
+
+# Directory where currently installed AUR packages are stored
+export AUR_DIR="/home/build"
+
 # Decide whether or not to upgrade AUR Packages while rebuilding
 export AUR_UPGRADE=true
 
@@ -14,11 +26,18 @@ export AUR_UPGRADE=true
 # NOTE: AUR packages in the AUR_DIR will automatically be whitelisted
 export AUR_WHITELIST=()
 
-# Directory where currently installed AUR packages are stored
-export AUR_DIR="/home/build"
+
+#######################################################################
+####################### Backup / Restore Options ######################
+#######################################################################
 
 # Where to store the system backup
 export BACKUP_LOCATION="/usr/local/backup"
 
-# Directories in which broken symlinks should be searched
+
+#######################################################################
+####################### System Cleaning Options #######################
+#######################################################################
+
+# Directories in which broken symlinks should be searched for
 export SYMLINKS_CHECK=("/etc" "/home" "/opt" "/srv" "/usr")
